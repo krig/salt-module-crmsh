7631f1f8a0cc46db81a67147644a7377ed39cd1f
Kristoffer Grönlund <krig@koru.se>
2017-09-12 12:02:35 +0200

    Skeleton files for grains, modules, states

---
+++ salt/states/crmsh.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+'''
+Configure a Pacemaker cluster using crmsh
+=========================================
+
+Configure a Pacemaker cluster using the crmsh
+cluster shell.
+
+:depends: crmsh
+'''
+from __future__ import absolute_import
