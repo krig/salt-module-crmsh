e10c583754ec7ed50aadf48616cbafc712a2eb5b
liangxin1300 <XLiang@suse.com>
2017-12-07 13:35:44 +0800

    dev: add configure_property function

--- salt/modules/crmsh.py
+++ salt/modules/crmsh.py
@@ -9,8 +9,63 @@ cluster shell.
 :depends: crmsh
 '''
 from __future__ import absolute_import
-from salt.ext import six
 import re
+
+from salt.ext import six
+from salt.exceptions import CommandExecutionError
+
+
+def _dc():
+    '''
+    check whether we are DC node
+    '''
+    import socket
+
+    try:
+        res = __salt__['cmd.run_all']("crmadmin -D")
+    except:
+        return False
+    if 'stdout' in res.keys():
+        dc = res['stdout'].split()[-1]
+        if dc != socket.gethostname():
+            return False
+
+    return True
+
+
+def configure_property(prop):
+    '''
+    Set a cluster property
+
+    prop
+        format of each property should be \"key=value\", or a dict type
+
+    This function will reject to run if current node is not DC.
+
+    CLI Example:
+
+    .. code-block:: bash
+
+        salt '*' crmsh.configure_property prop='stonith-enabled=true'
+    '''
+    if not _dc():
+        raise CommandExecutionError("This function can only run at DC node")
+
+    cmd = ['crm', 'configure', 'property']
+
+    if isinstance(prop, dict):
+        for k, v in prop.items():
+            cmd.append("%s=%s" % (k, v))
+    elif isinstance(prop, six.string_types):
+        for item in re.split(',\s*|\s', prop):
+            if re.search('\w+=\w+', item):
+                cmd.append(item)
+            else:
+                raise CommandExecutionError("format of each property should be \"key=value\"")
+    else:
+        raise CommandExecutionError("invalid prop type: %s" % type(prop))
+
+    return __salt__['cmd.run_all'](cmd, output_loglevel='trace', python_shell=False)
 
 
 def configure_show(xml=False, changed=False, filter=None):
